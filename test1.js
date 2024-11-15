517f84f5bae91115c765aa062383ecf136abe36f
Ranjit Kale <152802946+ranjitgithub2001@users.noreply.github.com>
2024-11-15 22:42:13 +0530

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+"use strict"
+
+const a=1245;
+let b="Ranjit";
+var c="Kale"
+e="This is String"; 
+let var2=3540
+// console.log(typeof e);
+// let var1;
+// console.log(typeof var1);
+// console.log(typeof undefined);
+console.log(typeof null);//in JS null is standalone value
+//it defines emptiness
+//number(2 to the power 53),bigInt,string,null,boolean,symbol
+//ECMA
+//const
+
+
+
+// console.log(a);
+//Earlier JS was not frindly with scope
+//prefer not to use var bcoz of issue in block scope and functional socpe
+// a=2564
+// c="hvjhv"
+// console.log(c);
+
+
